0594f2d64c1c43b5def67d286cc55970df61a256
David Pavlovski <david.pavlovski@hotmail.com>
2022-02-08 00:33:48 +0100

    added notes object file

---
+++ src/notes.js
@@ -0,0 +1,116 @@
+const notes = {
+   octave_2: {
+      notes: [
+         { note: 'C1', isFlat: false, altName: 'C' },
+         { note: 'Db1', isFlat: true, altName: 'Db' },
+         { note: 'D1', isFlat: false, altName: 'D' },
+         { note: 'Eb1', isFlat: true, altName: 'Eb' },
+         { note: 'E1', isFlat: false, altName: 'E' },
+         { note: 'F1', isFlat: false, altName: 'F' },
+         { note: 'Gb1', isFlat: true, altName: 'Gb' },
+         { note: 'G1', isFlat: false, altName: 'G' },
+         { note: 'Ab1', isFlat: true, altName: 'Ab' },
+         { note: 'A1', isFlat: false, altName: 'A' },
+         { note: 'Bb1', isFlat: true, altName: 'Bb' },
+         { note: 'B1', isFlat: false, altName: 'B' }
+      ]
+   },
+   octave_2: {
+      notes: [
+         { note: 'C2', isFlat: false, altName: 'C' },
+         { note: 'Db2', isFlat: true, altName: 'Db' },
+         { note: 'D2', isFlat: false, altName: 'D' },
+         { note: 'Eb2', isFlat: true, altName: 'Eb' },
+         { note: 'E2', isFlat: false, altName: 'E' },
+         { note: 'F2', isFlat: false, altName: 'F' },
+         { note: 'Gb2', isFlat: true, altName: 'Gb' },
+         { note: 'G2', isFlat: false, altName: 'G' },
+         { note: 'Ab2', isFlat: true, altName: 'Ab' },
+         { note: 'A2', isFlat: false, altName: 'A' },
+         { note: 'Bb2', isFlat: true, altName: 'Bb' },
+         { note: 'B2', isFlat: false, altName: 'B' }
+      ]
+   },
+   octave_3: {
+      notes: [
+         { note: 'C3', isFlat: false, altName: 'C' },
+         { note: 'Db3', isFlat: true, altName: 'Db' },
+         { note: 'D3', isFlat: false, altName: 'D' },
+         { note: 'Eb3', isFlat: true, altName: 'Eb' },
+         { note: 'E3', isFlat: false, altName: 'E' },
+         { note: 'F3', isFlat: false, altName: 'F' },
+         { note: 'Gb3', isFlat: true, altName: 'Gb' },
+         { note: 'G3', isFlat: false, altName: 'G' },
+         { note: 'Ab3', isFlat: true, altName: 'Ab' },
+         { note: 'A3', isFlat: false, altName: 'A' },
+         { note: 'Bb3', isFlat: true, altName: 'Bb' },
+         { note: 'B3', isFlat: false, altName: 'B' }
+      ]
+   },
+   octave_4: {
+      notes: [
+         { note: 'C4', isFlat: false, altName: 'C' },
+         { note: 'Db4', isFlat: true, altName: 'Db' },
+         { note: 'D4', isFlat: false, altName: 'D' },
+         { note: 'Eb4', isFlat: true, altName: 'Eb' },
+         { note: 'E4', isFlat: false, altName: 'E' },
+         { note: 'F4', isFlat: false, altName: 'F' },
+         { note: 'Gb4', isFlat: true, altName: 'Gb' },
+         { note: 'G4', isFlat: false, altName: 'G' },
+         { note: 'Ab4', isFlat: true, altName: 'Ab' },
+         { note: 'A4', isFlat: false, altName: 'A' },
+         { note: 'Bb4', isFlat: true, altName: 'Bb' },
+         { note: 'B4', isFlat: false, altName: 'B' }
+      ]
+   },
+   octave_5: {
+      notes: [
+         { note: 'C5', isFlat: false, altName: 'C' },
+         { note: 'Db5', isFlat: true, altName: 'Db' },
+         { note: 'D5', isFlat: false, altName: 'D' },
+         { note: 'Eb5', isFlat: true, altName: 'Eb' },
+         { note: 'E5', isFlat: false, altName: 'E' },
+         { note: 'F5', isFlat: false, altName: 'F' },
+         { note: 'Gb5', isFlat: true, altName: 'Gb' },
+         { note: 'G5', isFlat: false, altName: 'G' },
+         { note: 'Ab5', isFlat: true, altName: 'Ab' },
+         { note: 'A5', isFlat: false, altName: 'A' },
+         { note: 'Bb5', isFlat: true, altName: 'Bb' },
+         { note: 'B5', isFlat: false, altName: 'B' }
+      ]
+   },
+   octave_6: {
+      notes: [
+         { note: 'C6', isFlat: false, altName: 'C' },
+         { note: 'Db6', isFlat: true, altName: 'Db' },
+         { note: 'D6', isFlat: false, altName: 'D' },
+         { note: 'Eb6', isFlat: true, altName: 'Eb' },
+         { note: 'E6', isFlat: false, altName: 'E' },
+         { note: 'F6', isFlat: false, altName: 'F' },
+         { note: 'Gb6', isFlat: true, altName: 'Gb' },
+         { note: 'G6', isFlat: false, altName: 'G' },
+         { note: 'Ab6', isFlat: true, altName: 'Ab' },
+         { note: 'A6', isFlat: false, altName: 'A' },
+         { note: 'Bb6', isFlat: true, altName: 'Bb' },
+         { note: 'B6', isFlat: false, altName: 'B' }
+      ]
+   },
+   octave_7: {
+      notes: [
+         { note: 'C7', isFlat: false, altName: 'C' },
+         { note: 'Db7', isFlat: true, altName: 'Db' },
+         { note: 'D7', isFlat: false, altName: 'D' },
+         { note: 'Eb7', isFlat: true, altName: 'Eb' },
+         { note: 'E7', isFlat: false, altName: 'E' },
+         { note: 'F7', isFlat: false, altName: 'F' },
+         { note: 'Gb7', isFlat: true, altName: 'Gb' },
+         { note: 'G7', isFlat: false, altName: 'G' },
+         { note: 'Ab7', isFlat: true, altName: 'Ab' },
+         { note: 'A7', isFlat: false, altName: 'A' },
+         { note: 'Bb7', isFlat: true, altName: 'Bb' },
+         { note: 'B7', isFlat: false, altName: 'B' }
+      ]
+   }
+};
+
+export default notes;
