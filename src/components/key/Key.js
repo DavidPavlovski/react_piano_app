39b41a1f806fde5ff5ac7631c627face37aebcf2
David Pavlovski <david.pavlovski@hotmail.com>
2022-02-18 19:21:42 +0100

    moved audio tag from Piano to Key component

--- src/components/key/Key.js
+++ src/components/key/Key.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import './key.css';
 
-function Key({ isFlat, altName, playNote, note }) {
+function Key({ isFlat, altName, playNote, note, pressedKeys }) {
    const handlePlayNote = () => {
       playNote(note);
    };
+   const keyPressed = pressedKeys.indexOf(note) !== -1 ? 'active' : '';
+   const noteIsFlat = isFlat ? 'black' : 'white';
    return (
       <div
          id={`key-${note}`}
-         className={`key ${isFlat ? 'black' : 'white'}`}
+         className={`key ${noteIsFlat} ${keyPressed}`}
          onClick={handlePlayNote}
       >
          <span className='note-name'>{altName}</span>
+         <audio id={note} src={`/notes/${note}.mp3`} />
       </div>
    );
 }
